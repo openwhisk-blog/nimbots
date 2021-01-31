6324def98e7661b869c7fb35cdf88f09ac0f06b8
Michele Sciabarra <michele@sciabarra.com>
2021-01-31 14:30:40 +0100

    forgot

---
+++ web/src/berger.ts
@@ -0,0 +1,41 @@
+// https://en.wikipedia.org/wiki/Round-robin_tournament
+export function bergerTable(teams, useDummy=false, dummy={}) {
+    
+    if (!Array.isArray(teams)) 
+        teams = Array.from({ length: teams }).map((_, i) => i);
+    else 
+        teams = [...teams]; // copy array to avoid side effects
+    if (teams.length % 2 !== 0)
+        teams.push(dummy);
+
+    const n = teams.length;
+    const numberOfRounds = n-1;
+    const gamesPerRound = n/2;
+
+    let columnA = teams.slice(0, gamesPerRound);
+    let columnB = teams.slice(gamesPerRound);
+    const fixed = teams[0];
+
+    return Array.from({length: numberOfRounds}).map((_, i) => {
+        let gameCount = 1;
+        let round = Array.from({length: gamesPerRound}).reduce((acc, _, k) => {
+            if (useDummy || (columnA[k] !== dummy && columnB[k] !== dummy)) {
+                acc.push({
+                    round: i+1,
+                    game: gameCount,
+                    teamA: columnA[k],
+                    teamB: columnB[k]
+                }); 
+                gameCount++;
+            }
+            return acc;
+        }, []);
+
+        // rotate elements
+        columnA = [fixed, columnB.shift(), ...columnA.slice(1)];
+        columnB.push(columnA.pop());
+        return round;
+    });
+}
+
+
